9f64d091b50013f953b8faafdd08b8212a994b9f
Matthew-Rayner <matthewrayner28@gmail.com>
2023-11-16 15:39:51 +0000

    Implemented placement delay and sequence randomization

--- PlaygroundGame/Assets/Scripts/LavaKingController.cs
+++ PlaygroundGame/Assets/Scripts/LavaKingController.cs
@@ -1,20 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.VFX;
 
+[Serializable]
+public struct Obstacle
+{
+    public GameObject obj;
+    public Sprite icon;
+}
+
 public class LavaKingController : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> obstacles;
+    [SerializeField] private List<Obstacle> obstacles;
+    [SerializeField] private List<Obstacle> placementList = new List<Obstacle>();
 
     [SerializeField] private LayerMask wallMask;
     [SerializeField] private LayerMask inverseWallMask;
 
+    [SerializeField] private float placementDelay = 5f;
+    [SerializeField] private bool canPlace = true;
+
+    private void Start()
+    {
+        RandomizeObstacles();
+    }
+
     void Update()
     {
         //Left mouse button click
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && canPlace)
         {
             //Creates a ray using mouse position
             Vector3 mousePosition = Input.mousePosition;
@@ -29,8 +46,10 @@ public class LavaKingController : MonoBehaviour
                 //Checks if there is space for the obstacle
                 if (!Physics.CheckBox(spawnPosition, new Vector3(0.5f, 0.5f, 0.5f), Quaternion.identity, inverseWallMask))
                 {
-                    //Spawns obstacle
-                    SpawnObstacle(spawnPosition);
+                    Obstacle obstacle = placementList[0];
+                    placementList.RemoveAt(0);
+                    Instantiate(obstacle.obj, spawnPosition, Quaternion.identity);
+                    placementList.Add(obstacle);
                 }
                 else
                 {
@@ -40,8 +59,21 @@ public class LavaKingController : MonoBehaviour
         }
     }
 
-    private void SpawnObstacle(Vector3 position)
+    private void RandomizeObstacles()
     {
-        Instantiate(obstacles[0], position, Quaternion.identity);
+        List<Obstacle> tempObstacles = obstacles;
+
+        for (int i = 0; i < 5; i++)
+        {
+            placementList.Add(tempObstacles[(int)(UnityEngine.Random.value * tempObstacles.Count)]);
+            obstacles.Remove(placementList[i]);
+        }
+    }
+
+    private IEnumerator PlacementDelay()
+    {
+        canPlace = false;
+        yield return new WaitForSeconds(placementDelay);
+        canPlace = true;
     }
 }
