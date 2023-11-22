d185b25f93ec1d553eca3b88b15dad1ba9aa7d20
Jakearooney <Jakeatrooney@gmail.com>
2023-11-22 17:37:14 +0000

    Recoded some of the UI behaviour and resetups the first half of the layouts.

--- PlaygroundGame/Assets/Scripts/UI/CanvasSwap.cs
+++ PlaygroundGame/Assets/Scripts/UI/CanvasSwap.cs
@@ -1,8 +1,40 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CanvasSwap : MonoBehaviour
 {
-	private void Update()
+	private string previousMenu;
+    private Dictionary<string, CanvasGroup> canvasGroups;
+
+    private Dictionary<string, GameObject> settingsPanels;
+
+    [SerializeField] private GameObject audioPanel;
+    [SerializeField] private GameObject controlsPanel;
+
+    private void Start()
+    {
+        // Initialize and populate the dictionary
+        canvasGroups = new Dictionary<string, CanvasGroup>
+        {
+            {"titlescreen", titleScreenUI},
+            {"mainmenu", mainMenuUI},
+            {"exitmenu", exitMenuUI},
+            {"tutorialmenu", tutorialMenuUI},
+            {"gamelobbymenu", gameLobbyUI},
+            {"gamescreenmenu", gameScreenUI},
+            {"settingsmenu", settingsMenuUI},
+            {"pausemenu", pauseMenuUI},
+            {"wingameplayermenu", winScreenPlayerUI},
+            {"wingameenemymenu", winScreenEnemyUI}
+        };
+
+        settingsPanels = new Dictionary<string, GameObject>
+        {
+            {"audio", audioPanel},
+            {"controls", controlsPanel }
+        };
+    }
+    private void Update()
 	{
 		PauseGame();
 	}
@@ -11,153 +43,73 @@ public class CanvasSwap : MonoBehaviour
 	//Canvas Active Group
 	[SerializeField] private CanvasGroup titleScreenUI;
 	[SerializeField] private CanvasGroup mainMenuUI;
+	[SerializeField] private CanvasGroup exitMenuUI;
+    [SerializeField] private CanvasGroup tutorialMenuUI;
 	[SerializeField] private CanvasGroup gameLobbyUI;
 	[SerializeField] private CanvasGroup gameScreenUI;
 	[SerializeField] private CanvasGroup settingsMenuUI;
 	[SerializeField] private CanvasGroup pauseMenuUI;
 	[SerializeField] private CanvasGroup winScreenPlayerUI;
 	[SerializeField] private CanvasGroup winScreenEnemyUI;
-
-	//Canvas UnActive Group
-	[SerializeField] private CanvasGroup titleScreenUIF;
-	[SerializeField] private CanvasGroup mainMenuUIF;
-	[SerializeField] private CanvasGroup gameLobbyUIF;
-	[SerializeField] private CanvasGroup gameScreenUIF;
-	[SerializeField] private CanvasGroup settingsMenuUIF;
-	[SerializeField] private CanvasGroup pauseMenuUIF;
-	[SerializeField] private CanvasGroup winScreenPlayerUIF;
-	[SerializeField] private CanvasGroup winScreenEnemyUIF;
 	#endregion
 
 	#region Active group data
-	public void TitleScreen()
-	{
-		titleScreenUI.alpha = 1;
-		titleScreenUI.interactable = true;
-		titleScreenUI.blocksRaycasts = true;
-	}
 
-	public void MainMenu()
-	{
-		mainMenuUI.alpha = 1;
-		mainMenuUI.interactable = true;
-		mainMenuUI.blocksRaycasts = true;
-	}
+    public void ToggleCanvasGroup(string name)
+    {
+        if (canvasGroups.TryGetValue(name, out CanvasGroup canvasGroup))
+        {
+            bool isActive = canvasGroup.alpha != 0;
+            canvasGroup.alpha = isActive ? 0 : 1;
+            canvasGroup.blocksRaycasts = !isActive;
+            canvasGroup.interactable = !isActive;
 
-	public void GameLobby()
-	{
-		gameLobbyUI.alpha = 1;
-		gameLobbyUI.interactable = true;
-		gameLobbyUI.blocksRaycasts = true;
-	}
-
-	public void GameScreen()
-	{
-		gameScreenUI.alpha = 1;
-		gameScreenUI.interactable = true;
-		gameScreenUI.blocksRaycasts = true;
-	}
-
-	public void SettingMenu()
-	{
-		settingsMenuUI.alpha = 1;
-		settingsMenuUI.interactable = true;
-		settingsMenuUI.blocksRaycasts = true;
-	}
-
-	public void PauseMenu()
-	{
-		pauseMenuUI.alpha = 1;
-		pauseMenuUI.interactable = true;
-		pauseMenuUI.blocksRaycasts = true;
-	}
-
-	public void WinGamePlayer()
-	{
-		winScreenPlayerUI.alpha = 1;
-		winScreenPlayerUI.interactable = true;
-		winScreenPlayerUI.blocksRaycasts = true;
-	}
-
-	public void WinGameEnemy()
-	{
-		winScreenEnemyUI.alpha = 1;
-		winScreenEnemyUI.interactable = true;
-		winScreenEnemyUI.blocksRaycasts = true;
-	}
+            if (isActive)
+            {
+                previousMenu = name;
+            }
+        }
+    }
+    public void PreviousMenu()
+    {
+        ToggleCanvasGroup(previousMenu);
+    }
 	#endregion
 
-	#region Deactive group
-	public void TitleScreenF()
-	{
-		titleScreenUIF.alpha = 0;
-		titleScreenUIF.interactable = false;
-		titleScreenUIF.blocksRaycasts = false;
-	}
-
-	public void MainMenuF()
-	{
-		mainMenuUIF.alpha = 0;
-		mainMenuUIF.interactable = false;
-		mainMenuUIF.blocksRaycasts = false;
-	}
-
-	public void GameLobbyF()
-	{
-		gameLobbyUIF.alpha = 0;
-		gameLobbyUIF.interactable = false;
-		gameLobbyUIF.blocksRaycasts = false;
-	}
-
-	public void GameScreenF()
-	{
-		gameScreenUIF.alpha = 0;
-		gameScreenUIF.interactable = false;
-		gameScreenUIF.blocksRaycasts = false;
-	}
-
-	public void SettingMenuF()
-	{
-		settingsMenuUIF.alpha = 0;
-		settingsMenuUIF.interactable = false;
-		settingsMenuUIF.blocksRaycasts = false;
-	}
-
-	public void PauseMenuF()
-	{
-		pauseMenuUIF.alpha = 0;
-		pauseMenuUIF.interactable = false;
-		pauseMenuUIF.blocksRaycasts = false;
-	}
-
-	public void WinGamePlayerF()
-	{
-		winScreenPlayerUIF.alpha = 0;
-		winScreenPlayerUIF.interactable = false;
-		winScreenPlayerUIF.blocksRaycasts = false;
-	}
-
-	public void WinGameEnemyF()
-	{
-		winScreenEnemyUIF.alpha = 0;
-		winScreenEnemyUIF.interactable = false;
-		winScreenEnemyUIF.blocksRaycasts = false;
-	}
-	#endregion
 
 	//Pause game with escape key
 	private void PauseGame()
 	{
-		if(Input.GetKeyDown(KeyCode.Escape))
+		if(Input.GetKeyDown(KeyCode.Escape) && gameScreenUI.alpha != 0)
 		{
-			PauseMenu();
-			GameScreenF();
+            ToggleCanvasGroup("pausemenu");
+            ToggleCanvasGroup("gamescreenmenu");
 		}
 	}
 
 	public void QuitGame()
 	{
-		Application.Quit();
-		Debug.Log("Game has quit");
+        Debug.Log("Game has quit");
+        Application.Quit();
 	}
+
+    public void TogglePanel(string panelname)
+    {
+        if (settingsPanels.TryGetValue(panelname, out GameObject gameObject))
+        {
+            bool isActive = gameObject.activeSelf;
+            gameObject.SetActive(!isActive);
+
+            if (panelname == "audio" && controlsPanel.activeSelf)
+            {
+                controlsPanel.SetActive(false);
+            }
+            else if (panelname == "controls" && audioPanel.activeSelf)
+            {
+                audioPanel.SetActive(false);
+            }
+        }
+    }
+
+
 }
