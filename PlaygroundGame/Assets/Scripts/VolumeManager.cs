935be1472d30780ec1df05ef5ee2f852e01f1198
Jakearooney <Jakeatrooney@gmail.com>
2023-12-04 17:44:27 +0000

    Removed any references to master slider, Actual final commit. (hopefully)

--- PlaygroundGame/Assets/Scripts/VolumeManager.cs
+++ PlaygroundGame/Assets/Scripts/VolumeManager.cs
@@ -5,32 +5,21 @@ using UnityEngine.UI;
 
 public class VolumeManager : MonoBehaviour
 {
-    public float masterVolume = 1.0f;
     public float musicVolume = 1.0f;
     public float soundEffectVolume = 1.0f;
 
-    public Slider masterSlider;
     public Slider musicSlider;
     public Slider soundEffectSlider;
 
     void Start()
     {
         // Load the saved volume settings
-        masterVolume = PlayerPrefs.GetFloat("masterVolume", 1.0f);
         musicVolume = PlayerPrefs.GetFloat("musicVolume", 1.0f);
         soundEffectVolume = PlayerPrefs.GetFloat("soundEffectVolume", 1.0f);
 
         // Initialize the slider values
-        masterSlider.value = masterVolume;
         musicSlider.value = musicVolume;
         soundEffectSlider.value = soundEffectVolume;
-    }
-
-    public void OnMasterVolumeChange()
-    {
-        masterVolume = masterSlider.value;
-        // Save the new master volume
-        PlayerPrefs.SetFloat("masterVolume", masterVolume);
     }
 
     public void OnMusicVolumeChange()
